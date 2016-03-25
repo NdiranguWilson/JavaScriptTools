a3cf6f83bc5bdbc37611629459ab7a6eb547f539
Wilson Ndirangu <wndirangu@cytonn.com>
2016-03-25 09:41:52 +0300

    karma set up

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,10 @@
+/**
+ * Cytonn Technologies
+ * @author Ndirangu Wilson <wndirangu@cytonn.com>
+ * Javascript Tools
+ *
+ */
+
 // include gulp
 var gulp = require('gulp');
 
@@ -7,7 +14,13 @@ var jshint = require('gulp-jshint');
 var sass = require('gulp-ruby-sass');
 // Requires the browserSync plugin
 var browserSync = require('browser-sync').create();
-// JS hint task
+//uglify var
+var uglify = require('gulp-uglify');
+
+/**
+ * A jshint task  which checks for errors in javascripts file
+ */
+
 gulp.task('jshint', function() {
   gulp.src('./src/scripts/*.js')
     .pipe(jshint())
@@ -16,7 +29,7 @@ gulp.task('jshint', function() {
 
 //sass task
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
   return sass('./src/scss/**/*.scss')
     .on('error', sass.logError)
     .pipe(gulp.dest('./src/css'));
@@ -31,10 +44,17 @@ gulp.task('browserSync', function() {
   });
 });
 
-//Watch task
-gulp.task('default',['browserSync', 'sass'], function(){
+//Watch task concatenated with other tasks that run simultaneously when gulp is run
+gulp.task('default', ['browserSync', 'sass'], function() {
   gulp.watch('./src/scss/**/*.scss', ['sass']);
   // Other watchers
   gulp.watch('./src/*.html', browserSync.reload);
   gulp.watch('./src/scripts/**/*.js', browserSync.reload);
+});
+
+//uglify task
+gulp.task('compress', function() {
+  return gulp.src('./src//scripts/**/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'));
 });
